32d67f53089ae56353b3ea1fcb5d12d2961ee176
Eliel Sobral <eliel.sobral@ambisig.com>
2021-06-05 19:25:06 -0300

    Project hr-cloud-gateway-server created

---
+++ hr-cloud-gateway-server/src/main/java/br/com/pronetdigital/hrcloudgatewayserver/HrCloudGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.pronetdigital.hrcloudgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrCloudGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrCloudGatewayServerApplication.class, args);
+	}
+
+}
---
+++ hr-cloud-gateway-server/src/test/java/br/com/pronetdigital/hrcloudgatewayserver/HrCloudGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pronetdigital.hrcloudgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrCloudGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
