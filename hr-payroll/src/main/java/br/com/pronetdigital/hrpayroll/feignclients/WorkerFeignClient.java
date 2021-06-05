aaaf1c992812127d73d5871aa59e0dc4b0dcf907
Eliel Sobral <eliel.sobral@ambisig.com>
2021-06-05 10:27:20 -0300

    Ribbon discontinued and Implementation Spring Cloud Load Balancer with HttpStatus error 503(searching solution)

--- hr-payroll/src/main/java/br/com/pronetdigital/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/br/com/pronetdigital/hrpayroll/feignclients/WorkerFeignClient.java
@@ -1,5 +1,7 @@
 package br.com.pronetdigital.hrpayroll.feignclients;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import br.com.pronetdigital.hrpayroll.entities.Worker;
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
+@LoadBalancerClient(name = "hr-worker")
 public interface WorkerFeignClient {
 
+	@LoadBalanced
 	@GetMapping(value = "/{id}")
 	ResponseEntity<Worker> findById(@PathVariable Long id);
 
