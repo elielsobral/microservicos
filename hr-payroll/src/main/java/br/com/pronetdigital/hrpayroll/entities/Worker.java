1a1d4d8cae0a7fcdd4dc56cb6fe4e6884508998c
Eliel Sobral <eliel.sobral@ambisig.com>
2021-06-04 23:58:29 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/br/com/pronetdigital/hrpayroll/entities/Worker.java
@@ -0,0 +1,15 @@
+package br.com.pronetdigital.hrpayroll.entities;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class Worker implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+
+}
